b1c1b583a4060da5e09b8887e2d0f0a2331bfb1d
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-11-02 16:29:15 +0000

    Added tests and methods for 5 and 15

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -12,12 +12,28 @@ describe('Javabuzz', function() {
         expect(javabuzz.isDivisibleByThree(3)).toBe(true);       
       });
 
+      it('divisible by 5', function() {
+        expect(javabuzz.isDivisibleByFive(5)).toBe(true);       
+      });
+
+      it('divisible by 3 & 5', function() {
+        expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);       
+      });
+
     });
 
 
     describe('knows when a number is NOT', function() {
       it('divisible by 3', function() {
       expect(javabuzz.isDivisibleByThree(1)).toBe(false); 
+    });
+
+    it('divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(1)).toBe(false); 
+    });
+
+    it('divisible by 3 & 5', function() {
+      expect(javabuzz.isDivisibleByFifteen(1)).toBe(false); 
     });
 
   });
--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -8,4 +8,12 @@ class Javabuzz {
     return this._isDivisibleBy(number, 3);
   }
 
+  isDivisibleByFive(number) {
+    return this._isDivisibleBy(number, 5);
+  }
+
+  isDivisibleByFifteen(number) {
+    return this._isDivisibleBy(number, 15);
+  }
+
 }
